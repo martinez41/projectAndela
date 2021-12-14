71f9adf74e1c9adc500125d9662d3ff5094d9410
martinez41 <58294572+martinez41@users.noreply.github.com>
2021-12-14 17:01:14 +0100

    Add the color theme Add font familly : Smooch Regular build the sing_in activity

--- app/src/main/java/com/tdms140/projectandela/MainActivity.kt
+++ app/src/main/java/com/tdms140/projectandela/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.tdms140.projectandela
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val handler = Handler()
+        Handler().postDelayed({
+            val intent = Intent(this,Sign_In::class.java)
+            startActivity(intent)
+        },2000)
     }
 }
